4705d11c93bfab8d2c3cf6566013fe45c71bf45f
ponytojas <daniel.villalobosdel@gmail.com>
2021-04-15 12:56:14 +0200

    💄  Finish the first part of Hierarchy

--- src/views/Hierarchy.vue
+++ src/views/Hierarchy.vue
@@ -3,59 +3,139 @@
     <div
       class="z-20 self-center justify-center align-middle items-center w-11/12 2xl:w-6/12 my-12 py-8 px-8 md:py-12 md:px-12 card border-gray-200 border hover:border-0 rounded-md"
     >
-      <p class="text-center text-5xl font-extralight">Tamaño</p>
-      <div class="flex flex-row flex-wrap justify-evenly mt-12">
-        <div class="w-8/12 h-56 border-2 rounded-lg flex flex-row card">
-          <div class="flex flex-col w-3/12 bg-gray-300 h-full">
-            <img
-              class="h-full rounded-lg rounded-r-none"
-              src="../assets/images/flat.jpg"
-            />
-          </div>
-          <div class="flex flex-col w-9/12 h-full">
-            <div class="flex flex-row h-auto w-full">
-              <div class="flex flex-col">
-                <div class="flex flex-row h-3/6 w-full">
-                  <p class="pt-4 pl-4 text-2xl font-semibold">
-                    Piso en Carrer de la Murta
-                  </p>
-                </div>
-                <div class="flex flex-row h-3/6 w-full pl-4 mb-4 pt-2">
-                  <p>
-                    Piso recien reformado disponible para alquilar en una zona
-                    envidiable de Valencia
-                  </p>
-                </div>
-              </div>
+      <p class="text-center text-5xl font-extralight">
+        Tamaño y grosor de fuente
+      </p>
+      <div class="flex flex-col w-full flex-wrap items-center mt-12">
+        <div class="flex flex-row mb-2">
+          <span class="material-icons text-red-500 text-5xl"
+            >sentiment_dissatisfied</span
+          >
+        </div>
+        <div class="flex flex-row">
+          <div class="mx-auto w-8/12 h-56 border-2 rounded-lg flex flex-row card">
+            <div class="flex flex-col w-3/12 bg-gray-300 h-full">
+              <img
+                class="h-full rounded-lg rounded-r-none"
+                src="../assets/images/flat.jpg"
+              />
             </div>
-            <div class="flex flex-row h-full w-full bg-blue-50">
-              <div class="flex flex-col w-full h-full">
-                <div class="flex flex-row w-full h-full">
-                  <div class="flex flex-col w-6/12 h-full">
-                    <div class="flex flex-row h-full w-full">
-                      <div class="pt-3 pl-3">
-                        <p class="text-base">
-                          <span class="text-lg">600€/mes</span>
-                        </p>
-                      </div>
-                    </div>
-                    <div class="flex flex-row h-full w-full">
-                      <div class="pt-3 pl-3">
-                        <p class="text-sm">Estancia mínima de 6 meses</p>
-                      </div>
-                    </div>
+            <div class="flex flex-col w-9/12 h-full">
+              <div class="flex flex-row h-auto w-full">
+                <div class="flex flex-col">
+                  <div class="flex flex-row h-3/6 w-full">
+                    <p class="pt-4 pl-4 text-2xl font-semibold">
+                      Piso en Carrer de la Murta
+                    </p>
                   </div>
-                  <div class="flex flex-col w-6/12 h-full">
-                    <div
-                      class="px-4 py-2 mx-auto my-auto bg-blue-400 w-8/12 sm:w-4/12 md:w-6/12 lg:w-10/12 cursor-pointer text-white rounded hover:bg-blue-600"
-                    >
-                      <p class="text-center w-full">Contactar</p>
-                    </div>
+                  <div class="flex flex-row h-3/6 w-full pl-4 mb-4 pt-2">
+                    <p>
+                      Piso recien reformado disponible para alquilar en una zona
+                      envidiable de Valencia
+                    </p>
                   </div>
                 </div>
               </div>
+              <div class="flex flex-row h-full w-full bg-gray-50">
+                <div class="flex flex-col w-full h-full">
+                  <div class="flex flex-row w-full h-full">
+                    <div class="flex flex-col w-6/12 h-full">
+                      <div class="flex flex-row h-full w-full">
+                        <div class="pt-3 pl-3">
+                          <p class="text-base">
+                            <span class="text-lg">600€/mes</span>
+                          </p>
+                        </div>
+                      </div>
+                      <div class="flex flex-row h-full w-full">
+                        <div class="pt-3 pl-3">
+                          <p class="text-sm">Estancia mínima de 6 meses</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="flex flex-col w-6/12 h-full">
+                      <div
+                        class="px-4 py-2 mx-auto my-auto bg-purple-600 w-8/12 sm:w-4/12 md:w-6/12 lg:w-10/12 cursor-pointer text-white rounded hover:bg-purple-800"
+                      >
+                        <p class="text-center w-full">Contactar</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
+        </div>
+        <div class="flex flex-row mt-10 mb-2">
+          <span class="material-icons text-green-500 text-5xl"
+            >sentiment_satisfied</span
+          >
+        </div>
+        <div class="flex flex-row">
+          <div class="mx-auto w-8/12 h-56 border-2 rounded-lg flex flex-row card ">
+            <div class="flex flex-col w-3/12 bg-gray-300 h-full">
+              <img
+                class="h-full rounded-lg rounded-r-none"
+                src="../assets/images/flat.jpg"
+              />
+            </div>
+            <div class="flex flex-col w-9/12 h-full">
+              <div class="flex flex-row h-auto w-full">
+                <div class="flex flex-col">
+                  <div class="flex flex-row h-3/6 w-full">
+                    <p class="pt-4 pl-4 text-2xl font-bold text-gray-700">
+                      Piso en Carrer de la Murta
+                    </p>
+                  </div>
+                  <div
+                    class="flex flex-row font-light text-gray-900 h-3/6 w-full pl-4 mb-4 pt-2"
+                  >
+                    <p>
+                      Piso recien reformado disponible para alquilar en una zona
+                      envidiable de Valencia
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="flex flex-row h-full w-full bg-gray-50">
+                <div class="flex flex-col w-full h-full">
+                  <div class="flex flex-row w-full h-full">
+                    <div class="flex flex-col w-6/12 h-full">
+                      <div class="flex flex-row h-full w-full">
+                        <div class="pt-3 pl-3">
+                          <p class="text-base">
+                            <span class="text-lg font-bold text-gray-800"
+                              >600€<span
+                                class="text-base font-light text-gray-600"
+                                >/mes</span
+                              ></span
+                            >
+                          </p>
+                        </div>
+                      </div>
+                      <div class="flex flex-row h-full w-full">
+                        <div class="pt-3 pl-3">
+                          <p class="text-sm text-gray-600 font-light">
+                            Estancia mínima de 6 meses
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="flex flex-col w-6/12 h-full">
+                      <div
+                        class="px-4 py-2 mx-auto my-auto bg-purple-600 w-8/12 sm:w-4/12 md:w-6/12 lg:w-10/12 cursor-pointer text-white rounded hover:bg-purple-800"
+                      >
+                        <p class="text-center w-full">Contactar</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="flex flex-row mt-16 px-20">
+          <p class="text-gray-700 italic">"Los cambios en la fuente, tanto de grosor como de tamaño de letra permite desenfatizar cierta información y dar prioridad en la jerarquía de los elementos a determinadas partes de un texto. En este ejemplo, es más importante el precio y la zona que el tiempo de estancia mínimo, ya que son los factores donde más queremos llamar la atención del usuario."</p>
         </div>
       </div>
     </div>

b5b57d90b76a71688f4c88be734ea3985864d14f
Daniel Villalobos del Baño <ponytojas@Daniels-MacBook-Pro.local>
2021-04-15 18:51:42 +0200

    ✨ A new example

--- src/views/Hierarchy.vue
+++ src/views/Hierarchy.vue
@@ -13,7 +13,9 @@
           >
         </div>
         <div class="flex flex-row">
-          <div class="mx-auto w-8/12 h-56 border-2 rounded-lg flex flex-row card">
+          <div
+            class="mx-auto w-8/12 h-56 border-2 rounded-lg flex flex-row card"
+          >
             <div class="flex flex-col w-3/12 bg-gray-300 h-full">
               <img
                 class="h-full rounded-lg rounded-r-none"
@@ -72,7 +74,9 @@
           >
         </div>
         <div class="flex flex-row">
-          <div class="mx-auto w-8/12 h-56 border-2 rounded-lg flex flex-row card ">
+          <div
+            class="mx-auto w-8/12 h-56 border-2 rounded-lg flex flex-row card"
+          >
             <div class="flex flex-col w-3/12 bg-gray-300 h-full">
               <img
                 class="h-full rounded-lg rounded-r-none"
@@ -135,7 +139,82 @@
           </div>
         </div>
         <div class="flex flex-row mt-16 px-20">
-          <p class="text-gray-700 italic">"Los cambios en la fuente, tanto de grosor como de tamaño de letra permite desenfatizar cierta información y dar prioridad en la jerarquía de los elementos a determinadas partes de un texto. En este ejemplo, es más importante el precio y la zona que el tiempo de estancia mínimo, ya que son los factores donde más queremos llamar la atención del usuario."</p>
+          <p class="text-gray-700 italic">
+            "Los cambios en la fuente, tanto de grosor como de tamaño de letra
+            permite desenfatizar cierta información y dar prioridad en la
+            jerarquía de los elementos a determinadas partes de un texto. En
+            este ejemplo, es más importante el precio y la zona que el tiempo de
+            estancia mínimo, ya que son los factores donde más queremos llamar
+            la atención del usuario."
+          </p>
+        </div>
+      </div>
+    </div>
+    <div
+      class="z-20 self-center justify-center align-middle items-center w-11/12 2xl:w-6/12 my-12 py-8 px-8 md:py-12 md:px-12 card border-gray-200 border hover:border-0 rounded-md"
+    >
+      <p class="text-center text-5xl font-extralight">
+        Enfatizar desenfatizando
+      </p>
+      <div class="flex flex-col w-full flex-wrap items-center mt-12">
+        <div class="flex flex-row w-full">
+          <span class="material-icons text-red-500 text-5xl mx-auto"
+            >sentiment_dissatisfied</span
+          >
+        </div>
+        <div class="flex flex-row w-full mt-2">
+          <div class="w-full bg-gray-50 h-16 flex flex-row shadow border card">
+            <div class="flex flex-col w-1/5 h-full">
+              <img
+                class="h-12 w-12 my-auto ml-6"
+                src="../assets/images/logo.png"
+              />
+            </div>
+            <div class="flex flex-col w-4/5 h-full">
+              <div class="flex flex-row justify-around align-middle my-auto">
+                <p class="font-semibold cursor-pointer">Localización</p>
+                <p class="font-semibold text-yellow-500 cursor-pointer">
+                  Contacto
+                </p>
+                <p class="font-semibold cursor-pointer">Proyectos</p>
+                <p class="font-semibold cursor-pointer">Precio</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="flex flex-row w-full mt-10">
+          <span class="material-icons text-green-500 text-5xl mx-auto"
+            >sentiment_satisfied</span
+          >
+        </div>
+        <div class="flex flex-row w-full mt-2">
+          <div class="w-full bg-gray-50 h-16 flex flex-row shadow border card">
+            <div class="flex flex-col w-1/5 h-full">
+              <img
+                class="h-12 w-12 my-auto ml-6"
+                src="../assets/images/logo.png"
+              />
+            </div>
+            <div class="flex flex-col w-4/5 h-full">
+              <div class="flex flex-row justify-around align-middle my-auto">
+                <p class="font-bold text-gray-400 cursor-pointer">
+                  Localización
+                </p>
+                <p class="font-bold text-yellow-500 cursor-pointer">Contacto</p>
+                <p class="font-bold text-gray-400 cursor-pointer">Proyectos</p>
+                <p class="font-bold text-gray-400 cursor-pointer">Precio</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="flex flex-row mt-16 px-20">
+          <p class="text-gray-700 italic">
+            "En ocasiones, es más sencillo enfatizar en ciertos elementos,
+            haciéndolo destacar respecto a los demás, simplemente haciendo que
+            el resto de elementos resalten menos y no resaltando más el que se
+            quiere enfatizar."
+          </p>
         </div>
       </div>
     </div>
